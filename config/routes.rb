1c089f94e9bb87616e51171fa0fa687900c399c4
Marcos Neves <marcos.neves@gmail.com>
2014-03-06 15:40:13 -0300

    layout inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tarefas::Application.routes.draw do
+  root 'tarefas#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
