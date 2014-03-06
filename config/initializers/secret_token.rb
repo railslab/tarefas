9aa883d60a51dd6ef234f364496cfcfd43cf1714
Marcos Neves <marcos.neves@gmail.com>
2014-03-06 15:21:23 -0300

    rails new tarefas -B

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tarefas::Application.config.secret_key_base = 'bc68da4e7d12470fb3f77d24d252bc60c08c9a933e818533348f5d49373468a8d2bf8b6e18f48a594e81e7bdbc4f23c0600e25d8d5101b05ccb160fc24c8b438'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tarefas::Application.config.session_store :cookie_store, key: '_tarefas_session'
